d91de508a8e148fbb0636a3d07a60f7492ad6ba7
monkof1926 <l_bjacobsen@hotmail.com>
2022-12-14 11:38:36 +0100

    change start page to Movie

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import {
   Outlet
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Home} from "./components/Home";
-import Movie from "./components/Movie";
+import { Home } from "./components/Home";
+import { Movie } from "./components/Movie";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+     { /*<Home />*/}
+      <Movie />
     </div>
   );
 }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navgationbar from "../object/navBar.jsx";
 
 const Movie = () => {
+      useEffect(() => {
+            // Code to run when the component is rendered or updated
+          });
       <div>
-      <h1>Hallo this is your movie search</h1>
       <Navgationbar />
+      <h1>Hallo this is your movie search</h1>
       <p>This is a movie</p>
       </div>
 }
-export default Movie;
+export {Movie};
