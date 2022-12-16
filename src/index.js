11d2af4809ac0aa30f05303920d6295c5acfbfb7
monkof1926 <l_bjacobsen@hotmail.com>
2022-12-16 12:27:51 +0100

    navbar things

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Home} from './components/Home';
+import {Movie} from './components/Movie';
+import {navBar} from './object/navBar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Routes>
+        <Route path='/'    element = {<navBar />} >
+          <Route index          element = {<Home/>} />
+          <Route path='Movie' element = {<Movie />} />
+        </Route>
+      </Routes>
       <App />
     </BrowserRouter>
   </React.StrictMode>
